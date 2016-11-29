968a9ffbea27833f548aac1fae076eb274356261
P.T <gofawer@gmail.com>
2016-11-29 23:24:34 +0100

    Create SDL window.

---
+++ CreateWindow/src/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <SDL.h>
+
+std::string getResPath( const std::string& assetName ) {
+    std::string basePath = SDL_GetBasePath();
+    return basePath.substr( 0, basePath.find_last_of( "\\/", basePath.length() - 2 ) + 1 ) + "/res/" + assetName;
+}
+
+int main()
+{
+    if( SDL_Init( SDL_INIT_VIDEO ) != 0 ) {
+        std::cout << "SDL init error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow( "Hello SDL Window", 0, 0, 300, 200, SDL_WINDOW_SHOWN );
+    if( window == nullptr ) {
+        std::cout << "Create window error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+    }
+
+    SDL_Renderer* render = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
+    if( render == nullptr ) {
+        SDL_DestroyWindow( window );
+        std::cout << "Renderer error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Surface* surface = SDL_LoadBMP( getResPath( "circle.bmp" ).c_str() );
+    if( surface == nullptr ) {
+        std::cout << "Surface error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Texture* texture = SDL_CreateTextureFromSurface( render, surface );
+    SDL_FreeSurface( surface );
+    if( texture == nullptr ) {
+        std::cout << "Texture error: " << SDL_GetError() << std::endl;
+    }
+
+    for( int i = 0; i < 3; i++ ) {
+        SDL_RenderClear( render );
+        SDL_RenderCopy( render, texture, nullptr, nullptr );    // Draw
+        SDL_RenderPresent( render );    // update screen
+        SDL_Delay( 1000 );
+    }
+
+    return 0;
+}
